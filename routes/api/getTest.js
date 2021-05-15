38082625093ea909ab2be5699d513a4cc57380e1
Edward Huang <ejhuang.2015@gmail.com>
2021-05-15 11:32:18 -0400

    Made a change in routing

--- routes/api/getTest.js
+++ routes/api/getTest.js
@@ -7,13 +7,13 @@ const { checkJwt } = require("../../utils/check-jwt");
 // =============================================================
 // Non-auth route example
 router.get("/public-message", (req, res) => {
-   const message = "The API doesn't require an access token to share this message.";
+   const message = { message: "The API doesn't require an access token to share this message." };
    res.status(200).send(message);
 });
 
 // Auth required example
 router.get("/protected-message", checkJwt, (req, res) => {
-   const message = "The API successfully validated your access token.";
+   const message = { message: "The API successfully validated your access token." };
    res.status(200).send(message);
 });
  
